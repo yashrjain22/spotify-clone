923d6c1aadf1a8efd314e7ca8683d127def2a23e
yash-raj-jain22 <yash.r.jain22@gmail.com>
2026-03-28 10:38:05 +0530

    add: PLAYBAR and added some songs to the songs folder

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello, lets get started with js");
+
+async function main() {
+
+
+    let song = await fetch("http://127.0.0.1:3000/songs");
+    let response = await song.text();
+    console.log(response);
+    let element = document.createElement("div");
+    div.innerHtml = response;
+    let tds = div.getElementByTagName("td")
+    console.log(tds)
+}
+main(); 
